191e431f9ce5cfbaa19a6e2e467b9eea339fc145
venkat-72 <pasumarthivenkatanaveen72@gmail.com>
2023-05-31 16:05:11 -0500

    Assignment

---
+++ 05-30-2023/index.js
@@ -0,0 +1,34 @@
+var a = 10;
+var b = 30;
+var c = a + b;
+
+ console.log(c);
+
+ let val = "123";
+function  value() {
+ let val = "456";
+ console.log(val);
+ }
+ value();
+ console.log(val);
+ 
+ var bike = {
+ model : {year:"2015", year1:"2016"},
+ type :["two wheeler","three wheeler"],
+ engine: "v6",
+ 
+ }
+ console.log(bike.type[0]);
+ console.log(bike.model.year);
+ console.log(bike.engine);
+ 
+ const ab = [12345];
+  ab.push(7890);
+  console.log(ab);
+  
+  var str1="batch";
+  var str2=32;
+  var str3 = "welcome to java" + "str1" + "str2";
+  console.log(str3);
+  var str4= `welcome to java class ${str1}${str2}`;
+  console.log(str4);
